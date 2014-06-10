cc802a792b590af2921e276e6bc9487da3d6f7ca
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-06-10 17:28:46 +0400

    do not show authAlert if logout

--- iSistemium/Classes/Views/STMNavigationController.m
+++ iSistemium/Classes/Views/STMNavigationController.m
@@ -7,6 +7,7 @@
 //
 
 #import "STMNavigationController.h"
+#import "STMAuthController.h"
 
 @interface STMNavigationController () <UIAlertViewDelegate>
 
@@ -33,7 +34,7 @@
 
 - (void)showAuthAlert {
     
-    if (!self.authAlert.visible) {
+    if (!self.authAlert.visible && [STMAuthController authController].controllerState == STMAuthSuccess) {
         [self.authAlert show];
     }
     
