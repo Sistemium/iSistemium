1a7a3e9b7edfd6dea8e7e2115bfba18ece282ff2
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-07-28 17:32:24 +0400

    add STMDebtsTVC
    #36

---
+++ iSistemium/Classes/Views/STMDebtsTVC.h
@@ -0,0 +1,13 @@
+//
+//  STMDebtsTVC.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 28/07/14.
+//  Copyright (c) 2014 Sistemium UAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STMDebtsTVC : UITableViewController
+
+@end
