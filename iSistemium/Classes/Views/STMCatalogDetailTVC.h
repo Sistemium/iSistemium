c23a6285f4b90204ed9623ddead383c42a999caa
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2015-03-02 13:51:50 +0300

    add master & detail classes

---
+++ iSistemium/Classes/Views/STMCatalogDetailTVC.h
@@ -0,0 +1,13 @@
+//
+//  STMCatalogDetailTVC.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 02/03/15.
+//  Copyright (c) 2015 Sistemium UAB. All rights reserved.
+//
+
+#import "STMFetchedResultsControllerTVC.h"
+
+@interface STMCatalogDetailTVC : STMFetchedResultsControllerTVC
+
+@end
