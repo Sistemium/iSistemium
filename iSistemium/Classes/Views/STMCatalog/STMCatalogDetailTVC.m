ce89b8ddab077007788cb8f9bd0f8600e067170c
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2015-03-10 15:23:19 +0300

    hide keyboard if select article

--- iSistemium/Classes/Views/STMCatalog/STMCatalogDetailTVC.m
+++ iSistemium/Classes/Views/STMCatalog/STMCatalogDetailTVC.m
@@ -327,6 +327,8 @@
     } else {
         self.selectedArticle = [self.resultsController objectAtIndexPath:indexPath];
     }
+    
+    [self.view endEditing:NO];
 
     [self showArticleInfoPopover];
     
