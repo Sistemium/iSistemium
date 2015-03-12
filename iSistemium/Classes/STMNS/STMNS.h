81d80e1c668ea02f2375a3cb01b4738386ce5da1
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2015-03-12 14:39:41 +0300

    make STMNS dir

---
+++ iSistemium/Classes/STMNS/STMNS.h
@@ -0,0 +1,16 @@
+//
+//  STMNS.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 12/03/15.
+//  Copyright (c) 2015 Sistemium UAB. All rights reserved.
+//
+
+#ifndef iSistemium_STMNS_h
+#define iSistemium_STMNS_h
+
+#include "STMEntityDescription.h"
+#include "STMFetchRequest.h"
+
+
+#endif
