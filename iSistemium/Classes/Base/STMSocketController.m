032bc0961d04e0f3d93d3ab646072de3040f53d8
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-01-01 17:09:54 +0300

    correct unsynced objects count

--- iSistemium/Classes/Base/STMSocketController.m
+++ iSistemium/Classes/Base/STMSocketController.m
@@ -920,7 +920,15 @@
 - (NSArray *)unsyncedObjectsArray {
     
     if (self.isAuthorized && [STMSocketController document].managedObjectContext) {
-        return [self.resultsControllers valueForKeyPath:@"@distinctUnionOfArrays.fetchedObjects"];
+        
+        NSArray *fetchedObjects = [self.resultsControllers valueForKeyPath:@"@distinctUnionOfArrays.fetchedObjects"];
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"checksum != currentChecksum"];
+        
+        fetchedObjects = [fetchedObjects filteredArrayUsingPredicate:predicate];
+        
+        return fetchedObjects;
+        
     } else {
         return nil;
     }
