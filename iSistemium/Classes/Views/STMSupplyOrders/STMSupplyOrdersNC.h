772d7b6115164abe9ae70849a8852c630eac9ab5
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-01-09 16:49:25 +0300

    add supplyOrders_iphone storyboard

---
+++ iSistemium/Classes/Views/STMSupplyOrders/STMSupplyOrdersNC.h
@@ -0,0 +1,13 @@
+//
+//  STMSupplyOrdersNC.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 09/01/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import "STMActionPopoverNC.h"
+
+@interface STMSupplyOrdersNC : STMActionPopoverNC
+
+@end
