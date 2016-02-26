87c7f8ac9acfaf6f69d626b83772b295ce06b7cb
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-02-26 16:18:29 +0300

    prevent infinite loop if got error

--- iSistemium/Classes/Base/STMSocketController.m
+++ iSistemium/Classes/Base/STMSocketController.m
@@ -657,13 +657,16 @@
     } else {
         
         errorString = @"response not a dictionary";
-        [self sendEvent:STMSocketEventInfo withStringValue:errorString];
+        NSLog(@"error: %@", data);
         
     }
     
     if (errorString) {
     
-        [[STMLogger sharedLogger] saveLogMessageWithText:errorString type:@"error"];
+        NSLog(@"error: %@", errorString);
+        
+        [self sendEvent:STMSocketEventInfo withStringValue:errorString];
+//        [[STMLogger sharedLogger] saveLogMessageWithText:errorString type:@"error"];
         
         if ([[errorString.lowercaseString stringByReplacingOccurrencesOfString:@" " withString:@""] isEqualToString:@"notauthorized"]) {
             [[STMAuthController authController] logout];
@@ -699,6 +702,7 @@
         
         [self sharedInstance].isSendingData = NO;
         [[self syncer] sendFinishedWithError:errorString];
+        [self sharedInstance].syncDataDictionary = nil;
 
     } else {
 
