795b94b08af0912546c8de2dc070b08525a9c9ca
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-03-01 17:11:59 +0300

    add STMJSPicking storyboard

---
+++ iSistemium/Classes/Views/STMJSPicking/STMJSPickingNC.h
@@ -0,0 +1,13 @@
+//
+//  STMJSPickingNC.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 01/03/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import "STMActionPopoverNC.h"
+
+@interface STMJSPickingNC : STMActionPopoverNC
+
+@end
---
+++ iSistemium/Classes/Views/STMJSPicking/STMJSPickingVC.h
@@ -0,0 +1,13 @@
+//
+//  STMJSPickingVC.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 01/03/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STMJSPickingVC : UIViewController
+
+@end
