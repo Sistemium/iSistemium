a3d43f7b527d8dcb5459616ce502a41b77439a16
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-05-22 18:20:16 +0300

    add STMNS.h

---
+++ iSistemium/Classes/STMNS/STMNS.h
@@ -0,0 +1,15 @@
+//
+//  STMNS.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 22/05/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#ifndef STMNS_h
+#define STMNS_h
+
+#import "STMCoreNS.h"
+
+
+#endif /* STMNS_h */
